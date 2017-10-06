a9132c5dbdf28c49b7d2bdc6727c51fbecde55d3
cuzox <calevime@gmail.com>
2017-10-05 20:36:59 -0400

    Removed gif from the supported image formats

--- server/utils.js
+++ server/utils.js
@@ -2,7 +2,7 @@ let utils = module.exports
 
 utils.imageFilter = (req, file, cb) => {
     // Accept image only
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/i)) {
         return cb(new Error('Only image files are allowed!'), false);
     }
     cb(null, true);

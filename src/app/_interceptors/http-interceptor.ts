e66aea6458d46bb2d04ee77120f2991854a0c476
cuzox <calevime@gmail.com>
2017-11-06 23:46:17 -0500

    Small change in homescreen. Dev setup now using server data

--- src/app/_interceptors/http-interceptor.ts
+++ src/app/_interceptors/http-interceptor.ts
@@ -7,11 +7,6 @@ export class JWTInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let currentUser = JSON.parse(localStorage.getItem('currentUser'))
-    if (req.url.includes(':4200/api')){
-      req = req.clone({
-        url: req.url.replace('4200','3000')
-      });
-    }
     if (currentUser && currentUser.token && req.url.includes('/api')){
       req = req.clone({
         setHeaders: {
@@ -21,5 +16,4 @@ export class JWTInterceptor implements HttpInterceptor {
     }
     return next.handle(req);
   }
-
 }

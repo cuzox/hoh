c5f31ca8ff7284c9e84e50ed98ea7246c303f919
MaiteRosalie <maite.rosalie@gmail.com>
2017-11-11 16:20:30 -0400

    asd

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Child } from './../_models/child';
 import { ChildService } from './../_services/child.service';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core'
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
    moduleId: module.id,
@@ -21,6 +23,19 @@ export class HomeComponent implements OnInit {
     this._cs.getAll().subscribe(children =>{
       this.children = children
     })
+  }
+  scroll(direction){
+    let parent = document.getElementById("scroll-X");
+    if (direction=='left'){
+      $('#scroll-X').animate({
+        scrollLeft: parent.scrollLeft + parent.offsetWidth
+      }, 1000);
+    }
+    if (direction=='right'){
+      $('#scroll-X').animate({
+        scrollLeft: parent.scrollLeft - parent.offsetWidth
+      }, 1000);
+    }
   }
 
 }

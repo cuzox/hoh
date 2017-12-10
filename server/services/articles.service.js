213c7b965ee9d99d0ef1c7337aac24e845dec1c9
cuzox <calevime@gmail.com>
2017-12-10 15:39:05 -0500

    Finished up carts

--- server/services/articles.service.js
+++ server/services/articles.service.js
@@ -16,73 +16,73 @@ module.exports = service
 
 
 function getAll() {
-    var deferred = Q.defer()
- 
-    db.articles.find().toArray(function (err, articles) {
-        if (err) deferred.reject(err.name + ': ' + err.message)
-        else deferred.resolve(articles)
-    })
- 
-    return deferred.promise
+  var deferred = Q.defer()
+
+  db.articles.find().toArray(function (err, articles) {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    else deferred.resolve(articles)
+  })
+
+  return deferred.promise
 }
 
 function getById(_id) {
-    var deferred = Q.defer()
- 
-    db.articles.findOne({ _id: _id}, (err, article) => {
-        if (err) deferred.reject(err.name + ': ' + err.message)
-        else if (article) deferred.resolve(article)
-        else deferred.reject(`Article of id ${_id} not in database`)
-    })
- 
-    return deferred.promise
+  var deferred = Q.defer()
+
+  db.articles.findOne({ _id: _id }, (err, article) => {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    else if (article) deferred.resolve(article)
+    else deferred.reject(`Article of id ${_id} not in database`)
+  })
+
+  return deferred.promise
 }
 
 function create(articleParams) {
-    var deferred = Q.defer()
- 
-    db.articles.findOne({ title: articleParams.title }, (err, article) => {
+  var deferred = Q.defer()
+
+  db.articles.findOne({ title: articleParams.title }, (err, article) => {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    else if (article) deferred.reject('Article already in database')
+    else {
+      articleParams._id = shortId.generate()
+      db.articles.insert(articleParams, (err, article) => {
         if (err) deferred.reject(err.name + ': ' + err.message)
-        else if (article) deferred.reject('Article already in database')
-        else {
-            articleParams._id = shortId.generate()
-            db.articles.insert( articleParams, (err, article) => {
-                if (err) deferred.reject(err.name + ': ' + err.message)
-                else deferred.resolve(article)
-            })
-        }
-    })
- 
-    return deferred.promise
+        else deferred.resolve(article)
+      })
+    }
+  })
+
+  return deferred.promise
 }
 
 function update(_id, articleParams) {
-    var deferred = Q.defer()
- 
-    db.articles.findById({ _id: _id }, (err, article) => {
-        if (err) deferred.reject(err.name + ': ' + err.message)
-        else if (article) updateArticle()
-        else deferred.reject('Article not in database')
+  var deferred = Q.defer()
+
+  db.articles.findOne({ _id: _id }, (err, article) => {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    else if (article) updateArticle()
+    else deferred.reject('Article not in database')
+  })
+
+  function updateArticle() {
+    delete articleParams._id
+    db.articles.update({ _id: _id }, { $set: articleParams }, (err, doc) => {
+      if (err) deferred.reject(err.name + ': ' + err.message)
+      else deferred.resolve('Article updated successfully')
     })
- 
-    function updateArticle() {
-        delete articleParams._id
-        db.articles.update({ _id: _id }, { $set: articleParams }, (err, doc) => {
-            if (err) deferred.reject(err.name + ': ' + err.message)
-            else deferred.resolve('Article updated successfully')
-        })
-    }
- 
-    return deferred.promise
+  }
+
+  return deferred.promise
 }
 
 function _delete(_id) {
-    var deferred = Q.defer()
- 
-    db.articles.remove({ _id: _id }, err => {
-        if (err) deferred.reject(err.name + ': ' + err.message)
-        else deferred.resolve('article deleted successfully')
-    })
- 
-    return deferred.promise
+  var deferred = Q.defer()
+
+  db.articles.remove({ _id: _id }, err => {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    else deferred.resolve('Article deleted successfully')
+  })
+
+  return deferred.promise
 }
--- server/services/carts.service.js
+++ server/services/carts.service.js
@@ -2,91 +2,131 @@ var Q = require('q')
 var mongojs = require('mongojs')
 var config = require('../config')
 var shortId = require('shortid')
-var db = mongojs(config.dbConnectionString, ['carts', 'users'])
+var db = mongojs(config.dbConnectionString, ['carts', 'users', 'children'])
 
 var service = {}
 
 service.getAll = getAll
 service.getById = getById
 service.create = create
-service.update = update
+service.updateAdd = updateAdd
+service.updateRm = updateRm
 service.delete = _delete
 
 module.exports = service
 
 
 function getAll() {
-    var deferred = Q.defer()
- 
-    db.carts.find().toArray(function (err, carts) {
-        if (err) deferred.reject(err.name + ': ' + err.message)
-        else deferred.resolve(carts)
-    })
- 
-    return deferred.promise
+  var deferred = Q.defer()
+
+  db.carts.find().toArray(function (err, carts) {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    else deferred.resolve(carts)
+  })
+
+  return deferred.promise
 }
 
 function getById(_id) {
-    var deferred = Q.defer()
- 
-    db.carts.findOne({ _id: _id}, (err, cart) => {
-        if (err) deferred.reject(err.name + ': ' + err.message)
-        else if (cart) deferred.resolve(cart)
-        else deferred.reject(`Cart of id ${_id} not in database`)
-    })
- 
-    return deferred.promise
+  var deferred = Q.defer()
+
+  db.carts.findOne({ _id: _id }, (err, cart) => {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    else if (cart) deferred.resolve(cart)
+    else deferred.reject(`Cart of id ${_id} not in database`)
+  })
+
+  return deferred.promise
 }
 
 function create(cartParams) {
-    var deferred = Q.defer()
-    if(!cartParams.userId) deferred.reject("Must include userId")
-    else db.carts.findOne({ userId: cartParams.userId }, (err, cart) => {
-        if (err) deferred.reject(err.name + ': ' + err.message)
-        else if (cart) deferred.reject('User already has a cart')
-        else db.users.findOne({ _id: cartParams.userId }, (err, user) => {
-            if (err) deferred.reject(err.name + ': ' + err.message)
-            else if(!user) deferred.reject("No such user with the provided id")
-            else {
-                cartParams._id = shortId.generate()
-                db.carts.insert( cartParams, (err, cart) => {
-                    if (err) deferred.reject(err.name + ': ' + err.message)
-                    else deferred.resolve(cart)
-                })
-            }
+  var deferred = Q.defer()
+  if (!cartParams.userId) deferred.reject("Must include userId")
+  else db.carts.findOne({ userId: cartParams.userId }, (err, cart) => {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    else if (cart) deferred.reject('User already has a cart')
+    else db.users.findOne({ _id: cartParams.userId }, (err, user) => {
+      if (err) deferred.reject(err.name + ': ' + err.message)
+      else if (!user) deferred.reject("No such user with the provided id")
+      else {
+        cartParams._id = shortId.generate()
+        cartParams.children = []
+        db.carts.insert(cartParams, (err, cart) => {
+          if (err) deferred.reject(err.name + ': ' + err.message)
+          else deferred.resolve(cart)
         })
+      }
     })
- 
-    return deferred.promise
+  })
+
+  return deferred.promise
 }
 
-function update(_id, cartParams) {
-    var deferred = Q.defer()
- 
-    db.carts.findById({ _id: _id }, (err, cart) => {
+function updateAdd(_id, childId) {
+  var deferred = Q.defer()
+
+  db.carts.findOne({ _id: _id }, (err, cart) => {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    else if (cart) {
+      db.children.findOne({ _id: childId }, (err, child) => {
         if (err) deferred.reject(err.name + ': ' + err.message)
-        else if (cart) updatecart()
-        else deferred.reject('Cart not in database')
-    })
- 
-    function updatecart() {
-        delete cartParams._id
-        db.carts.update({ _id: _id }, { $set: cartParams }, (err, doc) => {
-            if (err) deferred.reject(err.name + ': ' + err.message)
-            else deferred.resolve('Cart updated successfully')
-        })
+        else if (child) updatecart(cart)
+        else deferred.reject('Child not in database')
+      })
     }
- 
-    return deferred.promise
+    else deferred.reject('Cart not in database')
+  })
+
+  function updatecart(cart) {
+    let updatedCart = {}
+    if (cart.children.includes(childId)) { deferred.reject('Child already in cart'); return }
+    cart.children.push(childId)
+    updatedCart.children = cart.children
+    db.carts.update({ _id: _id }, { $set: updatedCart }, (err, doc) => {
+      if (err) deferred.reject(err.name + ': ' + err.message)
+      else deferred.resolve('Child added to cart successfully')
+    })
+  }
+
+  return deferred.promise
+}
+
+function updateRm(_id, childId) {
+  var deferred = Q.defer()
+
+  db.carts.findOne({ _id: _id }, (err, cart) => {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    else if (cart) {
+      db.children.findOne({ _id: childId }, (err, child) => {
+        if (err) deferred.reject(err.name + ': ' + err.message)
+        else if (child) updatecart(cart)
+        else deferred.reject('Child not in database')
+      })
+    }
+    else deferred.reject('Cart not in database')
+  })
+
+  function updatecart(cart) {
+    let updatedCart = {}
+    if (!cart.children.includes(childId)) { deferred.reject('Child not in cart'); return }
+    cart.children.splice(cart.children.indexOf(childId), 1)
+    updatedCart.children = cart.children
+    db.carts.update({ _id: _id }, { $set: updatedCart }, (err, doc) => {
+      if (err) deferred.reject(err.name + ': ' + err.message)
+      else deferred.resolve('Child removed from cart successfully')
+    })
+  }
+
+  return deferred.promise
 }
 
 function _delete(_id) {
-    var deferred = Q.defer()
- 
-    db.carts.remove({ _id: _id }, err => {
-        if (err) deferred.reject(err.name + ': ' + err.message)
-        else deferred.resolve('Cart deleted successfully')
-    })
- 
-    return deferred.promise
+  var deferred = Q.defer()
+
+  db.carts.remove({ _id: _id }, err => {
+    if (err) deferred.reject(err.name + ': ' + err.message)
+    else deferred.resolve('Cart deleted successfully')
+  })
+
+  return deferred.promise
 }
